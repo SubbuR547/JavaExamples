4e606e2e9e25ddda9c4a0363e7f2c83166d14dce
subburoyal419@gmail.com <subburoyal419@gmail.com>
2024-01-07 01:04:51 +0530

    [Story 12345]"Removed unwated comments."

--- src/com/java/practice/examples/SimpleAddition.java
+++ src/com/java/practice/examples/SimpleAddition.java
@@ -3,7 +3,6 @@ package com.java.practice.examples;
 public class SimpleAddition {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int a = 5;
 		int b = 6;
 		int c = a + b;
