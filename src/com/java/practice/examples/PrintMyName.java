f69ded988dea25283f3ec988ccd93d321058af36
subburoyal419@gmail.com <subburoyal419@gmail.com>
2024-01-07 00:53:36 +0530

    Implemented print and simple addtion task

---
+++ src/com/java/practice/examples/PrintMyName.java
@@ -0,0 +1,10 @@
+package com.java.practice.examples;
+
+public class PrintMyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Name is Subhash");
+	}
+
+}
---
+++ src/com/java/practice/examples/SimpleAddition.java
@@ -0,0 +1,14 @@
+package com.java.practice.examples;
+
+public class SimpleAddition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 6;
+		int c = a + b;
+		System.out.println("The sum of " + a + " and " + b + " is equals to : " + c);
+
+	}
+
+}
